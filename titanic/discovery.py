f1eb0ea75c746c2a945bbb095313170c97b39378
Paul Gustafik <paul.gustafik@gmail.com>
2018-03-30 22:09:01 +0100

    added simple discovery

--- titanic/discovery.py
+++ titanic/discovery.py
@@ -5,4 +5,18 @@ import numpy as np
 
 
 train = pd.read_csv('/home/paul/dev/kaggle_competitions/titanic/data/train.csv')
-train.head(10)
+train.head()
+
+# In[]
+train.describe()
+
+# In[]
+foo = train.sort_values(by=["Age"])['Age']
+foo.reset_index().Age.plot()
+
+# In[]
+myString = "Foo, Mr. Bar"
+def getTitle(name):
+    return name[name.find(",")+1:name.find(".")]
+
+train.title = train.Name.apply(getTitle)
