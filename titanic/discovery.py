a2e9f093714d572a7a36edf1eefda480f2d092a4
Paul Gustafik <paul.gustafik@gmail.com>
2018-04-10 20:11:08 +0100

    @pallogu introduced discovery.ipynb

--- titanic/discovery.py
+++ titanic/discovery.py
@@ -4,19 +4,25 @@ import pandas as pd
 import numpy as np
 
 
-train = pd.read_csv('/home/paul/dev/kaggle_competitions/titanic/data/train.csv')
+train = pd.read_csv('//Users/paul/Sites/kaggle_competitions/titanic/data/train.csv')
 train.head()
+train['Age'].fillna(train['Age'].mean(), inplace=True)
 
 # In[]
 train.describe()
+
+# In[]
+train['Sex'].value_counts()
 
 # In[]
 foo = train.sort_values(by=["Age"])['Age']
 foo.reset_index().Age.plot()
 
 # In[]
-myString = "Foo, Mr. Bar"
 def getTitle(name):
     return name[name.find(",")+1:name.find(".")]
 
-train.title = train.Name.apply(getTitle)
+train['Title'] = train.Name.apply(getTitle)
+
+# In[]
+train['Title'].value_counts()
