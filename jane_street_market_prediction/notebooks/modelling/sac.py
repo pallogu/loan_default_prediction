ba70801e82666e4a43f70a03e8ec96822a17f589
Paul Gustafik <paul.gustafik@gmail.com>
2021-01-18 08:30:49 +0000

    added sketch of softmax with temperature

--- jane_street_market_prediction/notebooks/modelling/sac.py
+++ jane_street_market_prediction/notebooks/modelling/sac.py
@@ -10,8 +10,13 @@ from tf_agents.trajectories.time_step import TimeStep
 import time
 import mlflow
 
+
+# +
 seed(42)
 set_seed(42)
+
+tf.keras.backend.set_floatx('float64')
+# -
 
 # ### Environments
 
@@ -54,24 +59,24 @@ actor_model = keras.Sequential([
     layers.Dense(
         actor_nn_arch[1],
         activation = "relu",
-        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1/actor_nn_arch[0], seed=1)
+        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1, seed=1)
     ),
     layers.Dropout(0.5),
     layers.Dense(
         actor_nn_arch[2],
         activation = "relu",
-        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1/actor_nn_arch[1], seed=2)
+        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1, seed=2)
     ),
     layers.Dropout(0.5),
     layers.Dense(
         actor_nn_arch[3],
         activation = "relu",
-        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1/actor_nn_arch[2], seed=3)
+        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1, seed=3)
     ),
     layers.Dense(
         actor_nn_arch[4],
         activation = "softmax",
-        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1/actor_nn_arch[3], seed=4)
+        kernel_initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1, seed=4)
     )
 ])
 
@@ -112,7 +117,7 @@ critic_model = keras.Sequential([
 # ## Hyperparameters
 
 avg_reward_step_size = 1e-3
-actor_step_size = 1e-6
+actor_step_size = 1e-5
 critic_step_size = 1e-5
 number_of_episodes = 2
 
@@ -144,6 +149,53 @@ def calculate_u_metric(df, model):
 
 # ## AC Agent
 
+def softmax(action_values, tau=1.0):
+    """
+    Args:
+        action_values (Numpy array): A 2D array of shape (batch_size, num_actions). 
+                       The action-values computed by an action-value network.              
+        tau (float): The temperature parameter scalar.
+    Returns:
+        A 2D array of shape (batch_size, num_actions). Where each column is a probability distribution over
+        the actions representing the policy.
+    """
+    ### START CODE HERE (~2 Lines)
+    # Compute the preferences by dividing the action-values by the temperature parameter tau
+    preferences = action_values/tau
+    # Compute the maximum preference across the actions
+    max_preference = np.max(preferences, axis=1)
+    ### END CODE HERE
+    
+    
+    # Reshape max_preference array which has shape [Batch,] to [Batch, 1]. This allows NumPy broadcasting 
+    # when subtracting the maximum preference from the preference of each action.
+    reshaped_max_preference = max_preference.reshape((-1, 1))
+    
+   
+    ### START CODE HERE (~2 Lines)
+    # Compute the numerator, i.e., the exponential of the preference - the max preference.
+    exp_preferences = np.exp(preferences - reshaped_max_preference)
+    # Compute the denominator, i.e., the sum over the numerator along the actions axis.
+    sum_of_exp_preferences = np.sum(exp_preferences, axis=1)
+    ### END CODE HERE
+    
+    # Reshape sum_of_exp_preferences array which has shape [Batch,] to [Batch, 1] to  allow for NumPy broadcasting 
+    # when dividing the numerator by the denominator.
+    reshaped_sum_of_exp_preferences = sum_of_exp_preferences.reshape((-1, 1))
+    
+    ### START CODE HERE (~1 Lines)
+    # Compute the action probabilities according to the equation in the previous cell.
+    action_probs = exp_preferences/reshaped_sum_of_exp_preferences
+    ### END CODE HERE
+    
+    
+    # squeeze() removes any singleton dimensions. It is used here because this function is used in the 
+    # agent policy when selecting an action (for which the batch dimension is 1.) As np.random.choice is used in 
+    # the agent policy and it expects 1D arrays, we need to remove this singleton batch dimension.
+    action_probs = action_probs.squeeze()
+    return action_probs
+
+
 # +
 class ACAgent():
     def __init__(self, **kwargs):
@@ -164,6 +216,8 @@ class ACAgent():
         
     def init(self, time_step):
         observation = time_step.observation
+        
+        tf.debugging.assert_all_finite(observation, "observation contains non finite number")
 
         action = np.random.choice([0, 1])
         self.prev_observation = observation
@@ -174,6 +228,9 @@ class ACAgent():
     def train(self, time_step):
         observation = time_step.observation
         reward = time_step.reward
+        
+        tf.debugging.assert_all_finite(observation, "observation contains non finite number")
+        tf.debugging.assert_all_finite(reward, "reward contains non finite number")
         
         self.update_avg_reward(reward)
         self.update_td_error(reward, observation)
@@ -193,15 +250,19 @@ class ACAgent():
         return action
         
     def update_avg_reward(self, observation_reward):
-        self.reward = (1-self.avg_reward_step_size)*self.reward + self.avg_reward_step_size * observation_reward
+        self.reward = np.array((1-self.avg_reward_step_size)*self.reward + self.avg_reward_step_size * observation_reward, dtype=np.float64)
+        assert not np.isnan(self.reward)
         
     def update_td_error(self, observation_reward, observation):        
-        self.delta = (
+        self.delta = np.array(
             observation_reward 
             - self.reward
             + self.critic_model(observation).numpy()[0][0]
-            - self.critic_model(self.prev_observation).numpy()[0][0]
+            - self.critic_model(self.prev_observation).numpy()[0][0],
+            dtype = np.float64
         )
+        
+        assert not np.isnan(self.delta)
         
     def update_critic_model(self, observation):
         with tf.GradientTape() as tape:
@@ -210,14 +271,22 @@ class ACAgent():
                 self.critic_model.trainable_variables
             )]
             
+            for tensor in grad:
+                tf.debugging.assert_all_finite(tensor, "critic gradient contains non finite number")
+
+            
             self.critic_optimizer.apply_gradients(
                 zip(grad, self.critic_model.trainable_variables),
                 experimental_aggregate_gradients=False
             )
             
+            for tensor in self.critic_model.trainable_variables:
+                tf.debugging.assert_all_finite(tensor, "critic contains non finite number")
+            
     def update_actor_model(self, observation):
         prev_action = self.prev_action
         with tf.GradientTape() as tape:
+            
             grad = [-1 * self.delta * g for g in tape.gradient(
                 tf.math.log(self.actor_model(observation)),
                 self.actor_model.trainable_variables
@@ -225,16 +294,27 @@ class ACAgent():
             
             last_layer_w = grad[-2].numpy()
             last_layer_w[:, prev_action] = 0
-            grad[-2] = tf.constant(last_layer_w, dtype=np.float32)
+            grad[-2] = tf.constant(last_layer_w, dtype=np.float64)
             
             last_layer_b = grad[-1].numpy()
             last_layer_b[prev_action] = 0
-            grad[-1] = tf.constant(last_layer_b, dtype=np.float32)
+            grad[-1] = tf.constant(last_layer_b, dtype=np.float64)
+            
+            try:
+                for tensor in grad:
+                    tf.debugging.assert_all_finite(tensor, "actor gradient contains non finite number")
+            except:
+                print("prediction", self.actor_model(observation))
+                print(self.actor_model.trainable_variables)
+                raise Exception("actor gradient contains non finite number")
 
             self.actor_optimizer.apply_gradients(
                 zip(grad, self.actor_model.trainable_variables),
                 experimental_aggregate_gradients=False
-            )   
+            )
+            
+            for tensor in self.actor_model.trainable_variables:
+                tf.debugging.assert_all_finite(tensor, "actor model contains non finite number")
 
 
 
@@ -345,11 +425,19 @@ def run_experiment():
 
 
 run_experiment()
-# -
-actor_model.trainable_variables()
+# +
+# actor_model(train[:20][["f_{i}".format(i=i) for i in range(40)] + ["weight"]].values). numpy()
 
-actor_model(train[:2][["f_{i}".format(i=i) for i in range(40)] + ["weight"]].values). numpy()
+# +
+# actor_model(train[:2][["f_{i}".format(i=i) for i in range(40)] + ["weight"]].values). numpy()
+# -
 
 actor_model.summary()
+
+# +
+# a = tf.constant(np.array([[2, 4],[6, 8]]), dtype=np.float64)
+# b = tf.constant(np.array([[4, 6],[8, 10]]), dtype=np.float64)
+# a/b
+# -
 
 
