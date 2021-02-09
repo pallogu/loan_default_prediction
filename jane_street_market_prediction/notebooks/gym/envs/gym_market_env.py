8d63ad59a120b55d5316518a21004f1b96179fb2
Paul Gustafik <paul.gustafik@gmail.com>
2021-02-09 18:49:41 +0000

    modified gym market env

--- jane_street_market_prediction/notebooks/gym/envs/gym_market_env.py
+++ jane_street_market_prediction/notebooks/gym/envs/gym_market_env.py
@@ -4,7 +4,7 @@ from gym import spaces
 import pandas as pd
 import numpy as np
 
-class CustomEnv(gym.Env):
+class MarketEnvDaily(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
@@ -38,9 +38,9 @@ class CustomEnv(gym.Env):
         observation = self.active_day_trades.iloc[self.counter + 1][self.features].values
 
         if self.include_weight:
-            reward = 0 if action == 0 else self.trades.iloc[self.counter][self.reward_column]*self.trades.iloc[self.counter][self.weight_column]
+            reward = 0 if action == 0 else self.active_day_trades.iloc[self.counter][self.reward_column]*self.active_day_trades.iloc[self.counter][self.weight_column]
         else:
-            reward = 0 if action == 0 else self.trades.iloc[self.counter][self.reward_column]
+            reward = 0 if action == 0 else self.active_day_trades.iloc[self.counter][self.reward_column]
 
         if reward > 0:
             reward = reward * self.reward_multiplicator
@@ -67,6 +67,73 @@ class CustomEnv(gym.Env):
         self.active_day_trades = self.trades[self.trades["date"] == self.selected_date]
 
         observation = np.array(self.active_day_trades.iloc[self.counter][self.features].values, dtype=np.float32)
+        self._episode_ended = False
+        return observation  # reward, done, info can't be included
+    
+    def render(self, mode='human'):
+        pass
+        
+    def close (self):
+        pass
+
+
+class MarketEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, **kwargs):
+        super(CustomEnv, self).__init__()
+        
+        self.trades = kwargs.get("trades")
+        self.features = kwargs.get("features")
+        self.reward_column = kwargs.get("reward_column")
+        self.weight_column = kwargs.get("weight_column")
+        self.reward_multiplicator = kwargs.get("reward_multiplicator", 1)
+        self.negative_reward_multiplicator = kwargs.get("negative_reward_multiplicator", 1)
+        self.include_weight = kwargs.get("include_weight", True)
+
+        self.counter = 0
+
+        self._episode_ended = False
+        
+        self.action_space = spaces.Discrete(2)
+        # Example for using image as input (can be channel-first or channel-last):
+        self.observation_space = spaces.Box(low=-100, high=100, shape=(len(self.features),), dtype=np.float32)
+
+    def step(self, action):
+        if self.counter == (len(self.trades) - 2):
+            self._episode_ended = True
+
+        observation = self.trades.iloc[self.counter + 1][self.features].values
+
+        if self.include_weight:
+            reward = 0 if action == 0 else self.trades.iloc[self.counter][self.reward_column]*self.trades.iloc[self.counter][self.weight_column]
+        else:
+            reward = 0 if action == 0 else self.trades.iloc[self.counter][self.reward_column]
+
+        if reward > 0:
+            reward = reward * self.reward_multiplicator
+
+        if reward < 0:
+            reward = reward * self.negative_reward_multiplicator
+
+        if self._episode_ended:
+            done = True
+        else:
+            done = False
+            
+        info = {
+            "day": self.selected_date,
+            "reward": reward
+        }
+
+        self.counter += 1
+        return observation, reward, done, info
+    
+    def reset(self):
+        self.counter = 0
+
+        observation = np.array(self.trades.iloc[self.counter][self.features].values, dtype=np.float32)
         self._episode_ended = False
         return observation  # reward, done, info can't be included
     
