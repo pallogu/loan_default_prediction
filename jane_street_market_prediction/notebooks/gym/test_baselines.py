ae15b8ccd2a600a77af567b85a13d4098ddd1de0
Paul Gustafik <paul.gustafik@gmail.com>
2021-02-13 21:24:44 +0000

    experiment 1

--- jane_street_market_prediction/notebooks/gym/test_baselines.py
+++ jane_street_market_prediction/notebooks/gym/test_baselines.py
@@ -9,8 +9,8 @@ from stable_baselines import PPO2
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 
-from notebooks.gym.callbacks.EvaluationCallback import EvaluationCallback
-from .envs.gym_market_env import MarketEnv
+from callbacks.EvaluationCallback import EvaluationCallback
+from envs.gym_market_env import MarketEnv
 
 # -
 
@@ -19,31 +19,19 @@ eval_df = pd.read_csv("../etl/val_dataset_after_pca.csv")
 
 # +
 reward_multiplicator = 100
-negative_reward_multiplicator = 100
+negative_reward_multiplicator = 100*1.0582972011771625
+
+features = [c for c in train.columns.values if "f_" in c] + ["feature_0", "weight"]
 
 train_py_env = MarketEnv(
     trades=train,
-    features=[c for c in train.columns.values if "f_" in c] + ["feature_0", "weight"],
+    features=features,
     reward_column="resp",
     weight_column="weight",
     include_weight=True,
     reward_multiplicator=reward_multiplicator,
     negative_reward_multiplicator=negative_reward_multiplicator
 )
-
-eval_py_env = MarketEnv(
-    trades=train,
-    features=[c for c in train.columns.values if "f_" in c] + ["feature_0", "weight"],
-    reward_column="resp",
-    weight_column="weight",
-    include_weight=True,
-    reward_multiplicator=1,
-    negative_reward_multiplicator=1
-)
-
-# +
-
-
 # -
 
 train_env = DummyVecEnv([lambda: train_py_env])
@@ -51,14 +39,28 @@ evaluation_callback = EvaluationCallback(verbose=0, eval_df=eval_df, train_df=tr
 
 # %%time
 with mlflow.start_run():
-    policy_kwargs = dict(act_fun=tf.nn.swish, net_arch=[128, 128, 64, 32])
-    gamma = 0.01
-    learning_rate = 1e-6
+    num_features =len(features)
+    policy_kwargs = dict(act_fun=tf.nn.swish, net_arch=[3*num_features, 2*num_features, num_features])
+
+    
+    kwargs = {
+        "policy": MlpPolicy,
+        "env": train_env,
+        "learning_rate": 0.0001947227876477586,
+        "gamma": 0.2,
+        "nminibatches": int(2048/128),
+        "noptepochs": 5,
+        "ent_coef": 0.0005567548347339001,
+        "n_steps": 2048,
+        "lam": 0.9,
+        "cliprange":0.1,
+        "policy_kwargs": policy_kwargs
+    }
 
     mlflow.set_tag("agent_type", "PPO")
-    mlflow.log_param("policy", "mlp")
-    mlflow.log_param("policy_kwargs", policy_kwargs)
-    mlflow.log_param("gamma", gamma)
-    mlflow.log_param("learning_rate", learning_rate)
-    model = PPO2(MlpPolicy, train_env, verbose=0, gamma=gamma, learning_rate=learning_rate, policy_kwargs=policy_kwargs)
+    mlflow.log_params(kwargs)
+
+    model = PPO2(**kwargs)
     model.learn(total_timesteps=train.shape[0] * 10, callback=evaluation_callback)
+
+
